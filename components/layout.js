46905e7832f88dfad636dfa0621e925a48d7a222
Precelerator (SCE) <70708347+Precelerator@users.noreply.github.com>
2020-10-22 16:55:59 +0200

    Adapt image links in css

--- components/layout.js
+++ components/layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
         <div>
             <Head>
                 <title>Precelerator - Der Community-Space am SCE</title>
-                <meta charset="utf-8" />
+                <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
                 {/* SEO Meta Tags */}
